63de7cf42a210feaf5a5f89b98a383ba7f0319b1
BLY123456 <2915462492@qq.com>
2020-06-10 10:57:31 +0800

    对用户模块进行DDL语句操作

---
+++ com.git/src/com/Git/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.Git.dao;
+
+import java.util.List;
+
+public class UserDao {
+
+	public void addUser(int id,String username,String password) {
+		
+	}
+    public void deletUser(int id) {
+		
+	}
+    public void updateUser(int id,String newUsername,String newPassword) {
+		
+	}
+    public List<Object> scanUser(int pageIndex,int pageSize){
+    	return null;
+    }
+}
